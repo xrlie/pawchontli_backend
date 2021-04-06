edb109019d5410086f47a34ed3f700a5c868f002
xrlie <c.aviles.rz@gmail.com>
2021-04-06 18:52:52 -0500

    ADD pawchontli DB & migrations

--- pawchontli/pawchontli/settings.py
+++ pawchontli/pawchontli/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pawchontli.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pawchontli',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
